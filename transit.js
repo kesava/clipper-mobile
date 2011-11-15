cb0122dac154cc6193493f1dbc192b45d443ab2e
Kesava Mallela <kesava@gmail.com>
2011-11-15 12:12:08 -0800

    backbone added

--- transit.js
+++ transit.js
@@ -1,29 +1,46 @@
-Pass = Backbone.Model.extend({
-	defaults: {
-		name: '',
-		price: ''
-	},
-	initialize: function(name, price) {
-		this.set({name: name, price: price})
-	},
-	price: function() {
-		this.get("price");
-	}
-});
+$(function(){
+	Pass = Backbone.Model.extend({
+		defaults: {
+			title: '',
+			month: '',
+			price: ''
+		},
+		initialize: function(name, price) {
+			this.set({title: name, price: price})
+		},
+		price: function() {
+			this.get("price");
+		}
+	});
 
-TransitAgency = Backbone.Model.extend({
-    defaults: {
-        name: '',
-        passes: []
-    },
-    initialize: function(name){
-        this.set({name: name});
-    },
-	addPass: function(pass){
-		var passes_arr = this.get("passes");
-		passes_arr.push(pass);
-		this.set({passes: passes_arr});
-	}
-});
+	TransitAgency = Backbone.Model.extend({
+	    defaults: {
+	        name: '',
+	        passes: []
+	    },
+	    initialize: function(name){
+	        this.set({name: name});
+	    },
+		addPass: function(pass){
+			var passes_arr = this.get("passes");
+			passes_arr.push(pass);
+			this.set({passes: passes_arr});
+		}
+	});
 
-var 
+	Person = Backbone.Model.extend({
+		defaults: {
+			passes: []
+		},
+		buyPass: function(newpass){
+			var passes_arr = this.get('passes');
+			if !(_.find(passes_arr, function(pass) {return ((pass.title == newpass.title) && (pass.month == newpass.month))})) {
+				passes_arr.push(pass);
+				this.set({passes: passes_arr});			
+			}
+		},
+	});
+
+
+
+});
