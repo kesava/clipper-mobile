8c569ebcbee067072ecfec45cb72f52b388c22fb
Kesava Mallela <kesava@gmail.com>
2011-11-21 17:25:24 -0800

    Added cash value

--- app/views/appview.js
+++ app/views/appview.js
@@ -7,9 +7,15 @@ AppView = Backbone.View.extend({
     
 	buyPass: function(event){
 		var transitagency = event.target.id;
-		var pass = {name: $('.buyform input[name=' + transitagency + ']:checked').data('name'), price: $('.buyform input[name=' + transitagency + ']:checked').data('price')};
+		if (transitagency == 'cash') {
+			var pass = {name: 'Cash Value', price: $('#cash-value').val()};
+		}
+		else {			
+			var pass = {name: $('.buyform input[name=' + transitagency + ']:checked').data('name'), price: $('.buyform input[name=' + transitagency + ']:checked').data('price')};
+		}
 		$('#transitservices').trigger('collapse');
 		$.mobile.changePage( "#mainpage", { transition: "flip"} );
+		$('#mypasses').animate({'backgroundColor' : '#ffffff'});
 		passes.add(pass);
 		return false;
 	}
